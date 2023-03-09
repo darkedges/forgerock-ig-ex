824514af1179a6771970ae96b93953513450bce3
DarkEdges <nirving.darkedges@darkedges.com>
2023-03-09 11:27:20 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#  forgerock-ig-ex
+
+Example project for Forge Rock Identity Gateway with OpenShift
---
+++ scripts/groovy/hello.groovy
@@ -0,0 +1,3 @@
+Response response = new Response(Status.OK)
+response.entity = java.net.InetAddress.getLocalHost().getHostName();
+return response
